04439c6fd12fc089a66e3ed3ca3b949050bcf974
Vishal Vasnani <vishalvasnani123@gmail.com>
2017-10-29 21:52:01 +0530

    Add argument passing example

---
+++ events/example2.js
@@ -0,0 +1,10 @@
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+myEmitter.on('event', function(a,b) {
+	console.log(a,b,this);
+});
+
+myEmitter.emit('event', 'a', 'b');
